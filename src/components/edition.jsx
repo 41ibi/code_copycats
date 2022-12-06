435dc333d164ced6774aa5a0a42a4cdd59b5342b
jgoldsmidt <julieta.gold@gmail.com>
2022-12-06 17:24:02 -0300

    edition jsx return null

--- src/components/edition.jsx
+++ src/components/edition.jsx
@@ -1,19 +1,19 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-import Layout from "../src/components/layout"
+import Layout from "./layout"
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import Avatar from '@mui/material/Avatar'
 
-import theme from "../src/theme"
+import theme from "../theme"
 
 import "../CETEIcean.css"
 
 export default function Edition({ data, pageContext }) {
-  return (
-    <Layout location="edition">
+  return (null
+    /*<Layout location="edition">
       <div dangerouslySetInnerHTML={{__html: pageContext.rawContent}}/>
       <List sx={{width: '100%',
                 maxWidth: 360,
@@ -35,7 +35,7 @@ export default function Edition({ data, pageContext }) {
         ))
       }
       </List>
-    </Layout>
+    </Layout>*/
   )
 }
 
