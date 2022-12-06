6821c1ec6c339b868711ce78c34216263850e5ee
jgoldsmidt <julieta.gold@gmail.com>
2022-12-06 19:12:22 -0300

    delete home

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -96,8 +96,6 @@ export default function PersistentDrawerLeft({children, location}) {
     setOpen(false);
   };
 
-  const pages = data.allFile.nodes.filter((item) => item.name !== 'home')
-  pages.unshift({name: 'home'})
 
   return (
     <StyledEngineProvider injectFirst>
